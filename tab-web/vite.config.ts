f5691277a2ae2f4c66c754e37476a69ca4eae936
deslrey <deslre0381@gmail.com>
2026-04-21 22:13:06 +0800

    first commit

---
+++ tab-web/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()]
+    })
+  ],
+  base: './',
+
+  build: {
+    outDir: 'dist',
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
+})
