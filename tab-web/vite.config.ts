92a750d980e4bfaeb4afa0e05813439993d3fff2
deslrey <deslre0381@gmail.com>
2026-04-26 17:11:13 +0800

    chore: resolve

--- tab-web/vite.config.ts
+++ tab-web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from "path"
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
       presets: [reactCompilerPreset()]
     })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: './',
 
   build: {
